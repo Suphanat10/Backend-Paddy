d360f9cb4d3d9a0afd5a27ad3512a20f32f3a19a
suphanat10 <s6504062620060@kmutnb.ac.th>
2026-01-09 15:51:52 +0700

    update origin

--- server.js
+++ server.js
@@ -23,13 +23,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3001",
-      "https://smart-paddy.space",
-    ],
-    credentials: true,
-  },
+    origin: "*"
+  }
 });
+
 
 app.set("trust proxy", true);
 
